9e8503054158ea120b26c611e8c399a8ca84c24c
Emin <krates01@gmail.com>
2023-07-10 23:09:53 +0300

    Sdelal Zadachu 10

--- Zadacha10/Program.cs
+++ Zadacha10/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+int Input(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = Input("Введите трёхзначное число: ");
+if (number > 99 & number < 1000)
+{
+    int number_s = number / 10 % 10;
+    Console.WriteLine(number_s);
+}
+else Console.WriteLine("Введено некорректное число");
+
+
